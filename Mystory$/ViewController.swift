7d5060cf89d348965118270685f97a4da1a0ce4e
Marlvin1 <marlvingore.edu@gmail.com>
2023-02-27 20:51:09 -0600

    required detail activity

---
+++ Mystory$/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mystory$
+//
+//  Created by Marlvin Goremusandu on 2/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
